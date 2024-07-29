a2530022323766c892252cfe2df12a4ff7f8b094
bilunabeel <bilunabeelmv@gmail.com>
2024-07-29 22:10:39 +0530

    add:footer

---
+++ src/Sections/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Footer_Bar from '../assets/Footer_Ribbon.svg';
+import Org from '../assets/Org.svg';
+
+import {FaSquareYoutube} from 'react-icons/fa6';
+import {FaSquareInstagram} from 'react-icons/fa6';
+import {FaFacebookSquare} from 'react-icons/fa';
+import {FaSquareXTwitter} from 'react-icons/fa6';
+import {BsThreadsFill} from 'react-icons/bs';
+
+const Footer = () => {
+  return (
+    <section>
+      <img src={Footer_Bar} alt="" />
+      <div className="bg-theme_black py-16 padding-x">
+
+        <div className="flex justify-between max-md:flex-col max-md:items-center max-md:gap-5 max-container">
+          <img src={Org} className="h-5" />
+          <div className='text-white flex gap-2'>
+            <BsThreadsFill />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+            <FaSquareInstagram />
+            <FaSquareYoutube />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
--- src/Sections/Message.jsx
+++ src/Sections/Message.jsx
@@ -3,13 +3,13 @@ import Logo from '../assets/Logo.svg'
 
 const Message = () => {
   return (
-    <section className="z-10 bg-white  font-poppins">
+    <section className="z-10 bg-white  ">
       
       {/* //what's hope? */}
       <div className="max-container text-left flex justify-between items-center max-md:flex-col-reverse gap-16 padding-y padding-x">
         <div className="flex flex-col gap-5">
-          <h1 className="text-4xl font-bold">What's Hope?</h1>
-          <p className="font-light">
+          <h1 className="text-4xl font-mono ">What's Hope?</h1>
+          <p className="font-light font-poppins">
             The Festival of Hope is a beacon of optimism in a world often shrouded in uncertainty. It's a gathering where hearts unite, spirits soar, and the human capacity for resilience is celebrated. Whether it's a platform for young voices to be heard, a celebration of cancer survivors, or a community coming together to support those in need, the essence remains the same: to kindle hope, inspire action, and build a brighter future. Through music, art, storytelling, and shared experiences, the festival becomes a living testament to the enduring power of the human spirit.
           </p>
 

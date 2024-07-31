dd82783ef0881e0ea3836357f1c1262d5e81c40e
bilunabeel <bilunabeelmv@gmail.com>
2024-07-31 21:13:23 +0530

    add:image gallery

--- src/Sections/Gallery.jsx
+++ src/Sections/Gallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gallery = () => {
   return (
-    <div className="padding-y flex flex-col gap-4 justify-center">
+    <div className="padding-y flex flex-col gap-2 justify-center">
       <section className="scroll font-poppins flex justify-center items-center overflow-hidden  mask-gradient relative gap-2 max-container padding-x">
 
         <img src="./1.jpg" className="scroll-content h-36" alt="" />

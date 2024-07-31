a200eb40b35e5ae3e708d177eb913eeeb2b18e15
bilunabeel <bilunabeelmv@gmail.com>
2024-07-31 21:58:02 +0530

    add icon

--- src/Sections/Gallery.jsx
+++ src/Sections/Gallery.jsx
@@ -7,20 +7,20 @@ const Gallery = () => {
         <div className="scroll-content">
 
           <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
+          <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />
+          <img src="./7.jpg" className="scroll-content h-36" alt="" />
 
+          <img src="./8.jpg" className="scroll-content h-36" alt="" />
           <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
+          <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />
         </div>
 
       </section>
@@ -29,40 +29,38 @@ const Gallery = () => {
 
         <div className="scroll-content2">
 
+        <img src="./1.jpg" className="scroll-content h-36" alt="" />
+          <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />
+          <img src="./7.jpg" className="scroll-content h-36" alt="" />
+          <img src="./8.jpg" className="scroll-content h-36" alt="" />
           <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
-          <img src="./1.jpg" className="scroll-content h-36" alt="" />
+          <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />
         </div>
       </section>
 
       <section className="scroll font-poppins flex justify-center items-center overflow-hidden  mask-gradient relative  gap-2 max-container padding-x">
         <div className="scroll-content">
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />     
-
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />
-        <img src="./1.jpg" className="scroll-content h-36" alt="" />     
+        <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />
+          <img src="./7.jpg" className="scroll-content h-36" alt="" />
+          <img src="./8.jpg" className="scroll-content h-36" alt="" />
+          <img src="./1.jpg" className="scroll-content h-36" alt="" />
+          <img src="./2.jpg" className="scroll-content h-36" alt="" />
+          <img src="./3.jpg" className="scroll-content h-36" alt="" />
+          <img src="./4.jpg" className="scroll-content h-36" alt="" />
+          <img src="./5.jpg" className="scroll-content h-36" alt="" />
+          <img src="./6.jpg" className="scroll-content h-36" alt="" />     
         </div>
       </section>
     </div>

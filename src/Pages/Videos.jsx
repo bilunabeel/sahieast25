bb1be767cc6251e9ce049bfbd6cbf06d9dfc2ddc
bilunabeel <bilunabeelmv@gmail.com>
2024-08-05 10:42:32 +0530

    edit:make videos responsive

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -1,104 +1,85 @@
 import React from 'react';
+import {PiMedalBold, PiMedalFill, PiMedalLight} from 'react-icons/pi';
 
 const Videos = () => {
   return (
-    <div className="mt-16">
-      <div className=" padding-y padding-x">
+    <section className="mt-16 ">
+      <div className="padding-y padding-x">
+
         <h1 className="mb-8 text-left font-mono text-4xl text-theme_black">
-          Watch sahityotsav
+          watch sahityotsav
         </h1>
-        <div className="grid grid-cols-2 max-md:grid-cols-1 gap-x-12 ">
+        <div className="grid grid-cols-2 max-md:grid-cols-1 gap-6">
+          <div>
 
-          <div className=" h-full">
-            <div className="mb-6">
-              <div className="aspect-w-16 aspect-h-9">
+            <div className="mx-auto w-full flex flex-col gap-6 font-poppins">
 
-                <iframe
-                  src="https://www.youtube.com/embed/lE1lSbexl7k"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                />
+              <div>
+                <div className="aspect-w-16 aspect-h-9">
+
+                  <iframe
+                    className=""
+                    src="https://www.youtube.com/embed/lE1lSbexl7k"
+                    frameborder="0"
+                  />
+                </div>
+                <h1 className='mt-3 font-medium text-xl'>Stage 01 | Festival of Hope</h1>
               </div>
-              <h1 className="font-poppins font-medium max-md:text-x text-3xl mt-2">
-                Stage 01 | Festival of Hope
-              </h1>
-            </div>
-            <div>
-              <div className="aspect-w-16 aspect-h-9">
+              <div>
+                <div className="aspect-w-16 aspect-h-9">
 
-                <iframe
-                  src="https://www.youtube.com/embed/lE1lSbexl7k"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                />
+                  <iframe
+                    className=""
+                    src="https://www.youtube.com/embed/lE1lSbexl7k"
+                    frameborder="0"
+                  />
+                </div>
+                <h1 className='mt-3 font-medium text-xl'>Stage 01 | Festival of Hope</h1>
               </div>
-              <h1 className="font-poppins font-medium text-3xl mt-2">
-                Stage 02 | Festival of Hope
-              </h1>
             </div>
+
           </div>
-
-          {/* <div className="bg-black w-[1px] h-screen" /> */}
-
-          <div className=" h-16">
-            <div className="grid grid-cols-2 gap-8">
-
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-              <iframe
-                src="https://www.youtube.com/embed/lE1lSbexl7k"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-
+          <div className="w-full font-poppins text-theme_black flex flex-col">
+            <div className="grid grid-cols-2 gap-6">
+              <div className="aspect-w-16 aspect-h-9">
+                <iframe
+                  className="w-full h-full"
+                  src="https://www.youtube.com/embed/lE1lSbexl7k"
+                  frameborder="0"
+                />
+              </div>
+              <div className="aspect-w-16 aspect-h-9">
+                <iframe
+                  className="w-full h-full"
+                  src="https://www.youtube.com/embed/lE1lSbexl7k"
+                  frameborder="0"
+                />
+              </div>
+              <div className="aspect-w-16 aspect-h-9">
+                <iframe
+                  className="w-full h-full"
+                  src="https://www.youtube.com/embed/lE1lSbexl7k"
+                  frameborder="0"
+                />
+              </div><div className="aspect-w-16 aspect-h-9">
+                <iframe
+                  className="w-full h-full"
+                  src="https://www.youtube.com/embed/lE1lSbexl7k"
+                  frameborder="0"
+                />
+              </div><div className="aspect-w-16 aspect-h-9">
+                <iframe
+                  className="w-full h-full"
+                  src="https://www.youtube.com/embed/lE1lSbexl7k"
+                  frameborder="0"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="w-full flex bg-light_gray h-auto font-poppins" />
+    </section>
   );
 };
 

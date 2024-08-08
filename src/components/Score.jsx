a9205af6e4a2cb592134b7d49f6fa21649bce624
bilunabeel <bilunabeelmv@gmail.com>
2024-08-08 17:14:19 +0530

    add: animation to score

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import {GiTrophyCup} from 'react-icons/gi';
+import AnimatedCounter from './AnimatedCounter';
 
 const Score = ({scoreOn, setScoreOn}) => {
   const scoreRef = useRef (null);
@@ -34,7 +35,10 @@ const Score = ({scoreOn, setScoreOn}) => {
       </div>
 
       {scoreOn &&
-        <div ref={scoreRef} className="w-72 flex animate-fade-right animate-duration-100">
+        <div
+          ref={scoreRef}
+          className="w-72 flex animate-fade-right animate-duration-100"
+        >
           {/* <div className="w-0 h-0 border-r-[15px] border-r-theme_yellow border-b-[15px] border-b-transparent" /> */}
           <div className="p-8 w-72 ml-2 bg-theme_yellow font-poppins rounded-3xl flex flex-col">
             <h1 className="font-semibold text-xl text-left">Score Board</h1>
@@ -42,22 +46,90 @@ const Score = ({scoreOn, setScoreOn}) => {
             <div className="bg-black h-[.1px] w-full my-2" />
 
             <ul className="text-xl font-semibold">
-              <li>Kondotty <span className="text-2xl">000</span>{' '}</li>
-              <li>Areekode <span className="text-2xl">000</span>{' '}</li>
-              <li>Pulikkal <span className="text-2xl">000</span>{' '}</li>
+              <li className="flex items-center gap-3">
+                Kondotty
+                {' '}
+                <span className=" text-2xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Areekode
+                {' '}
+                <span className=" text-2xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+                {' '}
+              </li>
+              <li className="flex items-center gap-3">
+                Pulikkal
+                {' '}
+                <span className=" text-2xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+                {' '}
+              </li>
             </ul>
 
             <div className="bg-black w-full h-[.1px] my-2" />
 
             <ul className="text-lg">
-              <li>Malappuram <span className="text-xl">000</span></li>
-              <li>Edakkara <span className="text-xl">000</span></li>
-              <li>Nilambur <span className="text-xl">000</span></li>
-              <li>Kolathur <span className="text-xl">000</span></li>
-              <li>Manjeri West <span className="text-xl">000</span></li>
-              <li>Manjeri East <span className="text-xl">000</span></li>
-              <li>Pulikkal <span className="text-xl">000</span></li>
-              <li>Perinthalmanna <span className="text-xl">000</span></li>
+              <li className="flex items-center gap-3">
+                Malappuram
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Edakkara
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Nilambur
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Kolathur
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Manjeri West
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Manjeri East
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Pulikkal
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
+              <li className="flex items-center gap-3">
+                Perinthalmanna
+                {' '}
+                <span className=" text-xl">
+                  <AnimatedCounter endValue={300} duration={1000} />
+                </span>
+              </li>
             </ul>
 
           </div>
